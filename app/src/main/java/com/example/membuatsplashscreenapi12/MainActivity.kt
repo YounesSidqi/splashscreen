43532c0b3ec07d62e25d4b841e4a0c4b3f5afd17
YounesSidqi <almanafyounes@gmail.com>
2023-08-28 14:43:23 +0700

    first commit

---
+++ app/src/main/java/com/example/membuatsplashscreenapi12/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.membuatsplashscreenapi12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+    }
+}
